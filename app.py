a9b84291aa797b578fd2f580cf2435f95a406d0a
rayhanmuhammad14 <rayhan10915@gmail.com>
2025-07-07 11:05:41 +0800

    update

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
 from flask import Flask, request
+from pymongo import MongoClient
 import datetime
 
 app = Flask(__name__)
+
+# Ganti <username>, <password>, dan <cluster-url> sesuai dengan info MongoDB Atlas-mu
+client = MongoClient("mongodb+srv://randomrayhansmd:T9Kx6MUE3iTIotiF@cluster0.rj7k62j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
+# Pilih database dan collection
+db = client["logtracker"]
+log_collection = db["data"]
 
 @app.route('/')
 def track():
     ip = request.headers.get('X-Forwarded-For', request.remote_addr)
     ua = request.headers.get('User-Agent')
     t = datetime.datetime.now(datetime.timezone.utc)
-    with open('log.txt', 'a') as f:
-        f.write(f"[{t.isoformat()}] IP :{ip}, UA: {ua}\n")
+
+    log_entry = {
+        "timestamp": t,
+        "ip": ip,
+        "user_agent": ua
+    }
+
+    # Simpan ke MongoDB
+    log_collection.insert_one(log_entry)
+
     return "Thanks!!"
 
 if __name__ == "__main__":
